0837e28453f21cc3c5c5f4cd2caa1d0c8dd55aee
Matthew Tatsch <matthew.tatsch@microsoft.com>
2022-06-29 11:19:38 -0500

    Update setup script

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# .gitignore
+# Overview
 
-Starting with [GitHub's default .gitignore for Terraform](https://github.com/github/gitignore/blob/main/Terraform.gitignore).
+Generally following along with [Deploy Terraform using GitHub Actions to Azure](https://thomasthornton.cloud/2021/03/19/deploy-terraform-using-github-actions-into-azure/).
+
+# Prerequisites
+
+- Azure subscription
+- GitHub repo
+
+Following along with link referenced in [Overview](#overview).
+
+
+1. Add .gitignore - started with [GitHub's default .gitignore for Terraform](https://github.com/github/gitignore/blob/main/Terraform.gitignore).
+2. Run [PowerShell script in this repo](/scripts/powershell/SetUpTerraformRemoteState.ps1) to set up resource group, storage account, and service principal. 
---
+++ scripts/powershell/SetUpTerraformRemoteState.ps1
@@ -0,0 +1,28 @@
+### Set the following based on your preferences ###
+
+$LOCATION = "centralus"
+$PROJECT_ABBREV = "m11hghtftest" # lower case only
+$TFSTATE_ABBREV = "tfstate" # lower case only
+
+###################################################
+
+$SERVICE_PRINCIPAL_NAME = "${PROJECT_ABBREV}spn"
+$RESOURCE_GROUP_NAME = "${PROJECT_ABBREV}${TFSTATE_ABBREV}rg"
+$STORAGE_ACCOUNT_NAME = "${PROJECT_ABBREV}${TFSTATE_ABBREV}$(Get-Random -Maximum 999)st"
+$CONTAINER_NAME = "${PROJECT_ABBREV}${TFSTATE_ABBREV}"
+
+# Create resource group
+$rg = New-AzResourceGroup -Name $RESOURCE_GROUP_NAME -Location $LOCATION
+
+# Create Service Principal and grant access to resource group
+$sp = New-AzADServicePrincipal -DisplayName $SERVICE_PRINCIPAL_NAME
+New-AzRoleAssignment -ApplicationId $sp.AppId -RoleDefinitionName "Contributor" -Scope $rg.ResourceId
+
+# Show service principal client secret
+Write-Host $sp.PasswordCredentials.SecretText
+
+# Create storage account and container to store Terraform state
+$storageAccount = New-AzStorageAccount -ResourceGroupName $RESOURCE_GROUP_NAME -Name $STORAGE_ACCOUNT_NAME `
+                  -SkuName Standard_LRS -Location $LOCATION -AllowBlobPublicAccess $true
+
+New-AzStorageContainer -Name $CONTAINER_NAME -Context $storageAccount.context -Permission blob
