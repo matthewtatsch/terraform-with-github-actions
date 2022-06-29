f5f092f9aeaacec81c1af81c8c1e1700236aa5c3
Matthew Tatsch <matthew.tatsch@microsoft.com>
2022-06-29 13:13:32 -0500

    terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,37 +1,37 @@
 variable "project_name" {
-    type = "string"
-    default = "m11hsbx"
+  type    = string
+  default = "m11hsbx"
 }
 
 variable "abbreviations" {
-    type = map(string)
-    default = {
-      "resource_group" = "rg"
-    }
+  type = map(string)
+  default = {
+    "resource_group" = "rg"
+  }
 }
 
 variable "tags" {
-    type = map(string)
-    default = {
-      "environment" = "non-production"
-    }
+  type = map(string)
+  default = {
+    "environment" = "non-production"
+  }
 }
 
 variable "azure_region" {
-    type = map(string)
-    default = {
-        "primary" = "Central US"
-        "secondary" = "East US"
-    }
+  type = map(string)
+  default = {
+    "primary"   = "Central US"
+    "secondary" = "East US"
+  }
 }
 
 variable "tfbackend" {
-    description = "Terraform remote state storage account details"
-    type = map(string)
-    default = {
-        "resource_group_name" = "m11hghtftestrg"
-        "storage_account_name" = "m11hghtftest"
-        "container_name"       = "m11hghtftesttfstate"
-        "key"                  = "m11hghtftesttfstate.tfstate"
-    }
+  description = "Terraform remote state storage account details"
+  type        = map(string)
+  default = {
+    "resource_group_name"  = "m11hghtftestrg"
+    "storage_account_name" = "m11hghtftest"
+    "container_name"       = "m11hghtftesttfstate"
+    "key"                  = "m11hghtftesttfstate.tfstate"
+  }
 }
